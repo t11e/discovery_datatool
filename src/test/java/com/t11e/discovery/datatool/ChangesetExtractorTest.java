030d8549ef58e0435d563cc5fe8a74da736bc217
Eric/Terry <eric+terry@t11e.com>
2010-03-16 20:54:56 +0000

    Added (failing) test case with sample data for date range test.

--- src/test/java/com/t11e/discovery/datatool/ChangesetExtractorTest.java
+++ src/test/java/com/t11e/discovery/datatool/ChangesetExtractorTest.java
@@ -2,12 +2,17 @@ package com.t11e.discovery.datatool;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Resource;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,14 +21,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
   locations={"applicationContext-test.xml"})
 public class ChangesetExtractorTest
 {
-  @Autowired
-  private ChangesetExtractor extractor;
+  @Resource(name="emptyTable")
+  private ChangesetExtractor emptyTableExtractor;
+  @Resource(name="dateRange")
+  private ChangesetExtractor dateRangeExtractor;
 
   @Test
   public void testEmptyChangeset() throws Exception
   {
     final ByteArrayOutputStream os = new ByteArrayOutputStream();
-    extractor.getChangesetForRange(os, null, null);
+    emptyTableExtractor.getChangesetForRange(os, null, null);
 
     final CountingChangesetListener listener = new CountingChangesetListener();
     ChangesetStaxUtil.parseChangeset(
@@ -36,9 +43,23 @@ public class ChangesetExtractorTest
   }
 
   @Test
-  public void testDateRange()
+  public void testDateRange() throws Exception
   {
+    final ByteArrayOutputStream os = new ByteArrayOutputStream();
+    final DateFormat format = new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss.S");
+    final Date start = format.parse("2010-02-01-00.00.00.0000");
+    final Date end = format.parse("2010-02-01-00.00.03.0000");
 
+    dateRangeExtractor.getChangesetForRange(os, start, end);
+
+    final CountingChangesetListener listener = new CountingChangesetListener();
+    ChangesetStaxUtil.parseChangeset(
+      new ByteArrayInputStream(os.toByteArray()), listener);
+
+    Assert.assertEquals("Should have set items",
+      2, listener.getSetItemCount());
+    Assert.assertEquals("Should have no remove items",
+      0, listener.getRemoveItemCount());
   }
 
   @Test
